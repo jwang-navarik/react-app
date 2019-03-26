7f2c86dd372c641d6c4f671b69b48f2f29d79a27
Jack Wang <jwang@navarik.com>
2019-03-26 14:29:21 -0700

    Updates

---
+++ precache-manifest.559c07210cc848b9a4b7d83bf1e88d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "986fed61e496412132b5",
+    "url": "/react-app/static/js/main.53e7690b.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/react-app/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "986fed61e496412132b5",
+    "url": "/react-app/static/css/main.78bbcd81.chunk.css"
+  },
+  {
+    "revision": "a2d5712bf143e3a7702c1787a0d43696",
+    "url": "/react-app/index.html"
+  }
+];
